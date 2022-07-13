1985221c6b280aa8f98e51b0390386ae23c2e05f
SashaRPG <uasasharpg@gmail.com>
2022-07-13 21:00:06 +0300

    feat: added covers in data.json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 // import createHeader from "./header";
 
 import './style/index.css';
-// import './assets/44.jpeg';
-// import './assets/GitHub-Mark-120px-plus.png';
-// import './assets/rs_school.png'
 
-//import './style/normalize.css';
 
 // createHeader();
